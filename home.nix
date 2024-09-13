b7e95a03c014e8d308836635a79ad36e5af89555
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-09-12 20:32:29 -0400

    SUCCESS -- the '.zshrc' is now intitializing 'oh-my-posh' with my custom config files, which originates in this git repo.

--- home.nix
+++ home.nix
@@ -114,6 +114,8 @@
           "Alt-Z": "command:set wordwrap off,command:set softwrap off"
       }
     '';
+
+    ".config/oh-my-posh/zen.toml".source = dotfiles/.config/oh-my-posh/zen.toml;
     
   };
 
@@ -161,6 +163,12 @@
 
     zsh = {
       enable = true;
+
+      ## Make zsh init with a custom-written oh-my-posh config file written by home-manager (see `home.file = ...`).
+      initExtra = ''
+        eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config $HOME/.config/oh-my-posh/zen.toml)"
+      '';
+
       # shellAliases = {
       #   l = "ls -la";
       #   ll = "ls -l";
@@ -171,6 +179,9 @@
 
     oh-my-posh = {
       enable = true;
+      
+      ## This makes sure that zsh is installed/enabled, but prevents automatically adding `eval init zsh` to the '.zshrc', so that I can manually add it with `programs.zsh.initExtra` in a way that points to my own config file.
+      enableZshIntegration = false;
     };
 
     kitty = {
