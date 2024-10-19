f4842e3428c4707b79cca1bd16fc641712185026
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-10-19 15:31:09 -0400

    Python/uv -- finally updating the outdated flake/justfile, exact match to current redplanet versions.

--- docs/howto/python/template/flake.nix
+++ docs/howto/python/template/flake.nix
@@ -1,6 +1,6 @@
 {
 
-  description = "Python development environment with FHS and uv (github.com/astral-sh/uv).";
+  description = "Python development environment with uv (github.com/astral-sh/uv), requiring FHS.";
   # Credit/inspiration/reference: https://www.alexghr.me/blog/til-nix-flake-fhs/
 
 
@@ -17,7 +17,8 @@
         name = "fhs-shell";
         targetPkgs = pkgs: with pkgs; [
           uv
-          zlib    # required to make numpy work ("C-extensions" nonsense)
+          zlib    # required to make numpy work -- specifically, the error is `ImportError: libstdc++.so.6`
+          expat   # required for rasterio (which is required by rioxarray) -- specifically, the error is `ImportError: libexpat.so.1`
           just    # task-runner, see associated '.justfile'
         ];
       };
