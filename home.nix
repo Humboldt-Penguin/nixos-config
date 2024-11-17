a78b6ccd6ad748a62693c90d93a0362cedf4481d
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-11-17 17:46:20 -0500

    Zed / Meta -- Trying to upgrade to 0.161.1 or 0.161.2 is forcing me to recompile a bunch of stuff, so I'm pinning zed to the latest-working commit (0.160.7) for the time-being.

--- home.nix
+++ home.nix
@@ -8,6 +8,32 @@
   ...
 }:
 
+
+
+let
+  /*
+  The code below (in the `let` binding) allows you to force specific package(s) to use a certain commit hash. This is helpful when an update breaks a package or fails to compile, so you want to use an older version.
+    - Code/explanation found here: https://stackoverflow.com/a/79021669
+  */
+
+
+  /*
+  zed-editor 0.160.7
+    - 241117 self-note: Trying to upgrade to 0.161.1 or 0.161.2 is forcing me to recompile a bunch of stuff, so I'm pinning zed to the latest-working commit (0.160.7) for the time-being.
+    - https://github.com/NixOS/nixpkgs/commit/d0cab6aa5ab46c85a5489eec3bcf82c0a8faf6cc
+  */
+  pkgs_zeditor = import (pkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "d0cab6aa5ab46c85a5489eec3bcf82c0a8faf6cc";
+    sha256 = "uAaV3aARNf48XTsaPvFov2zhhv4I7vBQ7AZ8lcRO9IE=";
+  }) {
+    inherit (pkgs) system;
+  };
+in
+
+
+
 {
 
   home = {
@@ -95,12 +121,18 @@
         # uv
         just
 
-        ## text editors
-        zed-editor
+        # ## text editors
+        # zed-editor
 
         ## gui: media
         qbittorrent
-      ]);
+      ])
+      ++
+      (with pkgs_zeditor; [
+        ## text editors
+        zed-editor
+      ])
+      ;
 
 
 
