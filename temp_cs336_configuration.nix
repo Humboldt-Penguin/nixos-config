aea9dfcb3a5f2640eb1af3fe98fcb8c9f2a4dcf9
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-12-11 19:53:55 -0500

    mysql-workbench / mysql_jdbc (?) / mysql-shell (?)

--- temp_cs336_configuration.nix
+++ temp_cs336_configuration.nix
@@ -16,6 +16,9 @@
 
   environment.systemPackages =
     (with pkgs; [
+      mysql-workbench
+      mysql-shell   ## part of mysql windows installer tbf
+      mysql_jdbc    ## not sure if this is the correct way to load it...?
       jdk
       eclipses.eclipse-jee
     ]);
