718dfd46332d37aa75f48fca87de9e9801e9985d
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-01-15 23:15:02 -0500

    Full update

---
+++ nix-modules/apps/steam/system.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+
+  programs.steam = {
+    enable = true;
+  };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-unwrapped"
+    "steam-run"
+  ];
+
+}
