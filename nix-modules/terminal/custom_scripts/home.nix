e7389d5e04ba100678c4ca1e21c2a5d4e4a8fc82
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-04-29 21:20:44 -0400

    feat(inhibit/stay-awake): add progress-bar and move to its own script

--- nix-modules/terminal/custom_scripts/home.nix
+++ nix-modules/terminal/custom_scripts/home.nix
@@ -9,34 +9,24 @@
 }:
 
 {
+  /* NOTE: to make custom scripts in `~/.local/bin` available, you must add `export PATH="$HOME/.local/bin:$PATH"` to `programs.zsh.initExtra`. */
   home.file = {
 
     /*
-      NOTE: to make custom scripts in `~/.local/bin` available, you must add `export PATH="$HOME/.local/bin:$PATH"` to `programs.zsh.initExtra`.
       This command takes a file path like "foo.txt" and writes the SHA256 hash to a file named "foo.txt.sha256".
     */
     ".local/bin/write-sha256" = {
-
       executable = true;
-      text = ''
-        #!/usr/bin/env bash
-
-        if [ "$#" -ne 1 ]; then
-          echo "Usage: write-sha256 <filename>"
-          exit 1
-        fi
-
-        if [ -f "$1" ]; then
-          sha256sum "$1" > "$1".sha256
-        else
-          echo "Error: $1 is not a file"
-          exit 1
-        fi
-      '';
-
+      source = ./src/write-sha256;
     };
 
+    /*
+      This command prevents system sleeping for a specified amount of time (e.g. '20m', '1h', etc.)
+    */
+    ".local/bin/stay-awake" = {
+      executable = true;
+      source = ./src/stay-awake;
+    };
 
   };
-
 }
