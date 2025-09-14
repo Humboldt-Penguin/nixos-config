e2ba94118a54f76f80b5b8b1f56ad559b6776407
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-09-14 19:04:07 -0400

    fix(chromium): force X11 backend in order to fix bug with Chromium 140 on Wayland.

--- nix-modules/apps/chromium/home.nix
+++ nix-modules/apps/chromium/home.nix
@@ -16,7 +16,9 @@
       enable = true;
       package = pkgs.ungoogled-chromium;
 
+      /* Due to a bug with Chromium 140 on Wayland, we temporarily force X11 backend: https://chatgpt.com/c/68c74151-fa1c-8321-b482-e435367a0dcb */
       commandLineArgs = [
+        "--ozone-platform=x11"
         "--force-device-scale-factor=1"
       ];
 
